ebd111515862c88d512cb2c0aae6f266419c7423
PndP2357 <ssy@sosuyeob-ui-MacBookAir.local>
2024-03-02 16:02:57 +0900

    w07-example

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,27 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
+import FilterableProductTable from './components/FiltereableProductTable'
+
+const PRODUCTS = [
+  { category: "Fruits", price: "$1", stocked: true, name: "Apple" },
+  { category: "Fruits", price: "$1", stocked: true, name: "Dragonfruit" },
+  { category: "Fruits", price: "$2", stocked: false, name: "Passionfruit" },
+  { category: "Vegetables", price: "$2", stocked: true, name: "Spinach" },
+  { category: "Vegetables", price: "$4", stocked: false, name: "Pumpkin" },
+  { category: "Vegetables", price: "$1", stocked: true, name: "Peas" },
+];
+
 function App() {
+  //console.log(Date.now());
+
   const [count, setCount] = useState(0)
+
+  useEffect(() => {
+    console.log(Date.now());
+  }, [count]);
 
   return (
     <>
@@ -28,6 +45,8 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
+
+      <FilterableProductTable products={PRODUCTS}/>
     </>
   )
 }
